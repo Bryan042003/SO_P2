5def80643b16c1d78b540913548feb330db7692f
cbaeza16 <casey.baezac@gmail.com>
2024-10-08 09:57:41 -0600

    Fifo Upodate

--- src/app/algoritmos/FIFO.ts
+++ src/app/algoritmos/FIFO.ts
@@ -7,42 +7,21 @@ export class FIFO extends PageAlgorithm {
   }
 
   override referencePage(refPage: Page): [Page | null, number] {
-    // Incrementa el tiempo de uso de todas las páginas en la memoria
-    this.memory.forEach(page => page.lastAccess += 1);
-
     // Verifica si la página ya está en la memoria
     for (let page of this.memory) {
       if (page.pageId === refPage.pageId) {
-        page.lastAccess = 0; // Reinicia el tiempo de uso
         return [null, 0]; // No se reemplazó ninguna página
       }
     }
 
-    // Si la página no está en la memoria, reemplaza la página con más tiempo en memoria
-    let maxTime = Number.MAX_SAFE_INTEGER;
-    let maxPage: Page | null = null;
+    let replacedPage: Page | null = null;
 
-    this.memory.forEach(page => {
-      if (page.timestamp < maxTime) {
-        maxTime = page.timestamp;
-        maxPage = page;
-      }
-    });
-
-    // Almacena la dirección física de la página que será reemplazada
-    let physicalAddress = (maxPage as unknown as Page)?.physicalAddress;
-
-    // Elimina la página que será reemplazada
-    if (maxPage) {
-        console.log(`Reemplazando página`);
-      this.memory = this.memory.filter(p => p !== maxPage);
+    // Si la memoria está llena, reemplaza la página más antigua
+    if (this.memory.length >= this.memoryCapacity) {
+      replacedPage = this.memory.shift() || null; // Elimina la primera página
     }
-
-    // Agrega la nueva página a la memoria
-    refPage.physicalAddress = physicalAddress;
     this.memory.push(refPage);
 
-    // Retorna la página reemplazada y el código de resultado
-    return [maxPage, 1]; // Se reemplazó una página
+    return [replacedPage, 1];
   }
 }
